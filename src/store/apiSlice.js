d3dcc262f7dfc5e4288a24c775f12a79ee006f1f
Jamund Ferguson <jamund@gmail.com>
2023-02-13 15:08:14 +0000

    Setup basic API

---
+++ src/store/apiSlice.js
@@ -0,0 +1,8 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  endpoints: (builder) => {
+    return {};
+  },
+});
