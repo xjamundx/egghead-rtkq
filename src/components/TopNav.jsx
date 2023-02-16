52c5e501355276f1701db0411f3f1a87cd1d2fa5
Jamund Ferguson <jamund@gmail.com>
2023-02-16 09:32:05 +0000

    new nav

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -5,34 +5,17 @@ export function TopNav() {
   return (
     <nav className="topNav">
       <ul>
-        <li>
-          <Link className={location.pathname === "/" ? "selected" : ""} to="/">
-            Home
-          </Link>
+        <li className={location.pathname === "/" ? "selected" : ""}>
+          <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link
-            className={location.pathname === "/services" ? "selected" : ""}
-            to="/services"
-          >
-            Services
-          </Link>
+        <li className={location.pathname === "/services" ? "selected" : ""}>
+          <Link to="/services">Services</Link>
         </li>
-        <li>
-          <Link
-            className={location.pathname === "/dogs" ? "selected" : ""}
-            to="/dogs"
-          >
-            My Dogs
-          </Link>
+        <li className={location.pathname === "/dogs" ? "selected" : ""}>
+          <Link to="/dogs">My Dogs</Link>
         </li>
-        <li>
-          <Link
-            className={location.pathname === "/contact" ? "selected" : ""}
-            to="/contact"
-          >
-            Contact
-          </Link>
+        <li className={location.pathname === "/contact" ? "selected" : ""}>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
     </nav>
